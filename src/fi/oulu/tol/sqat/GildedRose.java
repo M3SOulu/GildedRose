822b8e457335b6becb8e437d17f39584692da005
moiltee <moilteep@gmail.com>
2015-09-25 14:14:35 +0300

    Test commit

--- src/fi/oulu/tol/sqat/GildedRose.java
+++ src/fi/oulu/tol/sqat/GildedRose.java
@@ -27,6 +27,10 @@ public class GildedRose {
         items.add(new Item("Conjured Mana Cake", 3, 6));
 
         updateQuality();
+        for(Item item: items)
+        {	
+        	System.out.println(item.getName()+"\t"+item.getSellIn()+"\t"+item.getQuality());
+        }
 }
 
 
@@ -35,76 +39,83 @@ public class GildedRose {
     {
         for (int i = 0; i < items.size(); i++)
         {
-            if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName())) 
-            {
-                if (items.get(i).getQuality() > 0)
-                {
-                    if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() - 1);
-                    }
-                }
-            }
-            else
-            {
-                if (items.get(i).getQuality() < 50)
-                {
-                    items.get(i).setQuality(items.get(i).getQuality() + 1);
-
-                    if ("Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
-                    {
-                        if (items.get(i).getSellIn() < 11)
-                        {
-                            if (items.get(i).getQuality() < 50)
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
-                            }
-                        }
-
-                        if (items.get(i).getSellIn() < 6)
-                        {
-                            if (items.get(i).getQuality() < 50)
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-            {
-                items.get(i).setSellIn(items.get(i).getSellIn() - 1);
-            }
-
-            if (items.get(i).getSellIn() < 0)
-            {
-                if (!"Aged Brie".equals(items.get(i).getName()))
-                {
-                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
-                    {
-                        if (items.get(i).getQuality() > 0)
-                        {
-                            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() - 1);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() - items.get(i).getQuality());
-                    }
-                }
-                else
-                {
-                    if (items.get(i).getQuality() < 50)
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() + 1);
-                    }
-                }
-            }
+            Item item = items.get(i);
+			updateItem(item);
         }
     }
+
+
+
+	public static void updateItem(Item item) {
+		if ((!"Aged Brie".equals(item.getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) 
+		{
+		    if (item.getQuality() > 0)
+		    {
+		        if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
+		        {
+		            item.setQuality(item.getQuality() - 1);
+		        }
+		    }
+		}
+		else
+		{
+		    if (item.getQuality() < 50)
+		    {
+		        item.setQuality(item.getQuality() + 1);
+
+		        if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
+		        {
+		            if (item.getSellIn() < 11)
+		            {
+		                if (item.getQuality() < 50)
+		                {
+		                    item.setQuality(item.getQuality() + 1);
+		                }
+		            }
+
+		            if (item.getSellIn() < 6)
+		            {
+		                if (item.getQuality() < 50)
+		                {
+		                    item.setQuality(item.getQuality() + 1);
+		                }
+		            }
+		        }
+		    }
+		}
+
+		if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
+		{
+		    item.setSellIn(item.getSellIn() - 1);
+		}
+
+		if (item.getSellIn() < 0)
+		{
+		    if (!"Aged Brie".equals(item.getName()))
+		    {
+		        if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
+		        {
+		            if (item.getQuality() > 0)
+		            {
+		                if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
+		                {
+		                    item.setQuality(item.getQuality() - 1);
+		                }
+		            }
+		        }
+		        else
+		        {
+		            item.setQuality(item.getQuality() - item.getQuality());
+		        }
+		    }
+		    else
+		    {
+		        if (item.getQuality() < 50)
+		        {
+		            item.setQuality(item.getQuality() + 1);
+		        }
+		    }
+		}
+	}
 
 }
--- src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -1,12 +1,71 @@
 package fi.oulu.tol.sqat.tests;
 
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-public class GildedRoseTest {
+import fi.oulu.tol.sqat.*;
 
+
+
+import org.junit.runners.*;
+
+@RunWith(Parameterized.class)
+public class GildedRoseTest 
+{
+
+	Item item;
+	int expectedSellIn;
+	int expectedQuality;
+	
+	
+	public GildedRoseTest(Item item,int expectedSellIn,int expectedQuality) 
+	{
+		this.item=item;
+		this.expectedSellIn = expectedSellIn;
+		this.expectedQuality = expectedQuality;
+				
+	}
+	
+	@Parameters (name = "{index} SellIn:{1} Quality:{2} ")
+	public static List<Object[]>myData()
+	{
+		return Arrays.asList(new Object[][]
+		{
+			{new Item("Aged Brie",2,0),1,1}
+		});
+		
+	}
+	
+	
 	@Test
-	public void testTheTruth() {
+	public void testTheTruth()
+	{
 		assertTrue(true);
 	}
+	
+	
+	@Test
+	public void testUpdateItemBrieSellIn2Quality0() 
+	{
+		
+		//Item item =new Item("Aged Brie", 2, 0);
+		GildedRose.updateItem(item);
+		
+		
+		assertEquals("SellIn not correct",expectedSellIn,item.getSellIn());
+		assertEquals("Quality not correct",expectedQuality,item.getQuality());
+	
+		
+	}
+	
+	
+	
+	
 }
