2faa85c66846e1ecbdbc00dfd5f5d62546e68730
somename <someemail>
2015-10-12 14:19:10 +0300

    besouro automatic message

--- src/fi/oulu/tol/sqat/GildedRose.java
+++ src/fi/oulu/tol/sqat/GildedRose.java
@@ -3,105 +3,102 @@ package fi.oulu.tol.sqat;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class GildedRose {
 
-	private static List<Item> items = null;
+	private List<Item> items = null;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-        System.out.println("OMGHAI!");
-		
-        items = new ArrayList<Item>();
-        items.add(new Item("+5 Dexterity Vest", 10, 20));
-        items.add(new Item("Aged Brie", 2, 0));
-        items.add(new Item("Elixir of the Mongoose", 5, 7));
-        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-        items.add(new Item("Conjured Mana Cake", 3, 6));
 
-        updateQuality();
-}
+		System.out.println("OMGHAI!");
 
+		List<Item> items = new ArrayList<Item>();
+		items.add(new Item("+5 Dexterity Vest", 10, 20));
+		items.add(new Item("Aged Brie", 2, 0));
+		items.add(new Item("Elixir of the Mongoose", 5, 7));
+		items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+		items.add(new Item("Conjured Mana Cake", 3, 6));
 
+		new GildedRose(items, 1);
+	}
 	
-    public static void updateQuality()
-    {
-        for (int i = 0; i < items.size(); i++)
-        {
-            if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName())) 
-            {
-                if (items.get(i).getQuality() > 0)
-                {
-                    if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() - 1);
-                    }
-                }
-            }
-            else
-            {
-                if (items.get(i).getQuality() < 50)
-                {
-                    items.get(i).setQuality(items.get(i).getQuality() + 1);
+	public GildedRose(List<Item> items, int qualityUpdateDays) {
+		this.items = items;
+		updateQuality(qualityUpdateDays);
+	}
 
-                    if ("Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
-                    {
-                        if (items.get(i).getSellIn() < 11)
-                        {
-                            if (items.get(i).getQuality() < 50)
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
-                            }
-                        }
+	public void updateQuality(int days) {
+		while (days > 0) {
+			System.out.println("Good morning Hero! Days left to survive: " + days);
+			for (Item item : items) {
 
-                        if (items.get(i).getSellIn() < 6)
-                        {
-                            if (items.get(i).getQuality() < 50)
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
-                            }
-                        }
-                    }
-                }
-            }
+				System.out.println("Before update: Item is " + item.getName() + ". Sell date " + item.getSellIn()
+						+ ". Quality " + item.getQuality());
 
-            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-            {
-                items.get(i).setSellIn(items.get(i).getSellIn() - 1);
-            }
+				String name = item.getName();
+				int quality = item.getQuality();
+				int sellIn = item.getSellIn();
 
-            if (items.get(i).getSellIn() < 0)
-            {
-                if (!"Aged Brie".equals(items.get(i).getName()))
-                {
-                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
-                    {
-                        if (items.get(i).getQuality() > 0)
-                        {
-                            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() - 1);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() - items.get(i).getQuality());
-                    }
-                }
-                else
-                {
-                    if (items.get(i).getQuality() < 50)
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() + 1);
-                    }
-                }
-            }
-        }
-    }
+				if (item.getQuality() > 0 || item.getSellIn() > 0) {
+					if (name.contains("Sulfuras")) {
+					} else if (name.contains("Conjured") && quality > 0) {
+						if (sellIn == 0) {
+							item.setQuality(quality - 4);
+						} else {
+							item.setQuality(quality - 2);
+							item.setSellIn(sellIn - 1);
+						}
+					} else if (name.contains("Backstage") && quality < 50) {
+						if (sellIn <= 10 && sellIn > 5) {
+							item.setQuality(quality + 2);
+							item.setSellIn(sellIn - 1);
+						} else if (sellIn <= 5 && sellIn > 0) {
+							item.setQuality(quality + 3);
+							item.setSellIn(sellIn - 1);
+						} else if (sellIn == 0) {
+							item.setQuality(0);
+						} else {
+							item.setQuality(quality + 1);
+							item.setSellIn(sellIn - 1);
+						}
+					} else if (name.contains("Aged")) {
+						if (quality < 50 && quality > 0) {
+							item.setQuality(quality + 1);
+							item.setSellIn(sellIn - 1);
+						}
+					} else {
+						if (sellIn == 0 && quality > 0) {
+							item.setQuality(quality - 2);
+						} else {
+							item.setQuality(quality - 1);
+							item.setSellIn(sellIn - 1);
+						}
+					}
+				}
+				if (quality == 0 && sellIn > 0) {
+					item.setSellIn(sellIn - 1);
+				}
 
+				System.out.println("After update: Item is " + item.getName() + ". Sell date " + item.getSellIn()
+						+ ". Quality " + item.getQuality());
+			}
+			days = days - 1;
+		}
+	}
+	
+	public List<Item> getItems() {
+		return this.items;
+	}
+	
+	public Item getItemByName(String name) {
+		for (Item item : items) {
+			if (item.getName().contains(name)) {
+				return item;
+			}
+		}
+		return null;
+	}
 }
