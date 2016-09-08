b24ce97ccd76cd305c02e06274390bed6a6ceb60
Ari Ruokamo <ari.ruokamo@gmail.com>
2016-09-08 19:13:59 +0300

    Threshold value tests for GildedRose updateEndOfTheDay().

--- src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -20,7 +20,19 @@ public class GildedRoseTest {
 //   Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
 //   Item("Conjured Mana Cake", 3, 6));
 
+	// Once the sell by date has passed, Quality degrades twice as fast
+	// The Quality of an item is never negative
+	// The Quality of an item is never more than 50
+	// "Aged Brie" actually increases in Quality the older it gets
+	// "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+	// "Backstage passes", like aged brie, increases in Quality as it's SellIn value approaches; 
+	// Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but 
+	// Quality drops to 0 after the concert.
+	
+	// Comments placed above the test method just for the clarity of the practise
+	
 	@Test
+	// Basic brie aging test
 	public void testUpdateEndOfDay_AgedBrie_Quality_10_11() {
 		// Arrange
 		GildedRose store = new GildedRose();
@@ -35,6 +47,121 @@ public class GildedRoseTest {
 		assertEquals(11, itemBrie.getQuality());
 	}
     
+	// Test for negative quality
+	@Test
+	public void testUpdateEndOfDay_NeverNegative_Quality_1_0() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Never negative", 1, 0) );
+		
+		// Act
+		store.updateEndOfDay();
+		store.updateEndOfDay();
+
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(0, itemBrie.getQuality());
+	}
+
+	// Quality maximum reached
+	@Test
+	public void testUpdateEndOfDay_AgedBrie_Quality_49_50() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Aged Brie", 1, 49) );
+		
+		// Act
+		store.updateEndOfDay();
+
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(50, itemBrie.getQuality());
+	}
+
+	// Quality increased by two
+	@Test
+	public void testUpdateEndOfDay_AgedBrie_Quality_10_15() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Aged Brie", 1, 10) );
+		
+		// Act
+		store.updateEndOfDay();
+		store.updateEndOfDay();
+		store.updateEndOfDay();
+
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(15, itemBrie.getQuality());
+	}
+	
+	// Sulfuras never sellIn test
+	@Test
+	public void testUpdateEndOfDay_Sulfuras_SellIn_1_1() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Sulfuras", 1, 1) );
+		
+		// Act; two days
+		store.updateEndOfDay();
+
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(1, itemBrie.getSellIn());
+	}
+
+	// Backstage pass +2 quality test
+	@Test
+	public void testUpdateEndOfDay_BP_Quality_1_3() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Backstage passes", 10, 1) );
+		
+		// Act; two days
+		store.updateEndOfDay();
+
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(3, itemBrie.getSellIn());
+	}
+
+	// Backstage pass +3 quality test
+	@Test
+	public void testUpdateEndOfDay_BP_Quality_1_4() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Backstage passes", 5, 1) );
+		
+		// Act; two days
+		store.updateEndOfDay();
+
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(4, itemBrie.getSellIn());
+	}
+
+	// Backstage pass 0 quality test
+	@Test
+	public void testUpdateEndOfDay_Sulfuras_Quality_1_0() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Backstage passes", 0, 10) );
+		
+		// Act; two days
+		store.updateEndOfDay();
+
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(0, itemBrie.getSellIn());
+	}
+
 	@Test
 	public void testUpdateEndOfDay() {
 		fail("Test not implemented");
