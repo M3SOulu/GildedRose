4b6590fff3e4268e5010330792c1915868b4d99e
juho <juhoo.kananen@gmail.com>
2016-10-13 18:16:28 +0300

    progress

--- src/fi/oulu/tol/sqat/GildedRose.java
+++ src/fi/oulu/tol/sqat/GildedRose.java
@@ -7,6 +7,9 @@ import java.util.List;
 public class GildedRose {
 
 	private static List<Item> items = null;
+	private static String SULFURAS = "Sulfuras, Hand of Ragnaros";
+	private static String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+	private static String BRIE = "Aged Brie";
 
 	public List<Item> getItems() {
 		return items;
@@ -20,11 +23,11 @@ public class GildedRose {
 		items = new ArrayList<Item>();
 	}
 	
-	private static boolean isSellInLessThanSix(Item item) {
+	private static boolean isSellInLessThanSixDays(Item item) {
 		return item.getSellIn() < 6;
 	}
 
-	private static boolean isSellInLessThanEleven(Item item) {
+	private static boolean isSellInLessThanElevenDays(Item item) {
 		return item.getSellIn() < 11;
 	}
 
@@ -32,7 +35,7 @@ public class GildedRose {
 		return item.getSellIn() < 0;
 	}
 
-	private static boolean isQualityAboveFifty(Item item) {
+	private static boolean isQualityLessThanFifty(Item item) {
 		return item.getQuality() < 50;
 	}
 
@@ -55,17 +58,35 @@ public class GildedRose {
 	private static void increaseQuality(Item item) {
 		item.setQuality(item.getQuality() + 1);
 	}
+
+	private static void qualityWhenSellInLessThanSixDaysAndQualityUnderFifty(Item item) {
+		if (isSellInLessThanSixDays(item))
+		     if (isQualityLessThanFifty(item))
+		         increaseQuality(item);
+	}
+
+	private static void qualityWhenSellInLessThanElevenDaysAndQualityUnderFifty(Item item) {
+		if (isSellInLessThanElevenDays(item))
+		     if (isQualityLessThanFifty(item))
+		         increaseQuality(item);
+	}
 	
     public static void updateEndOfDay()
     {
     	for(Item item:items)
         {
-			if ((!"Aged Brie".equals(item.getName())) && 
-            		!"Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) 
+    		 if (BACKSTAGE.equals(item.getName()))
+             {
+    			 qualityWhenSellInLessThanElevenDaysAndQualityUnderFifty(item);
+                 qualityWhenSellInLessThanSixDaysAndQualityUnderFifty(item);
+             }
+    		 
+    		 
+			if ((!BRIE.equals(item.getName())) && !BACKSTAGE.equals(item.getName())) 
             {
                 if (isQualityAboveZero(item))
                 {
-                    if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
+                    if (!SULFURAS.equals(item.getName()))
                     {
                         decreaseQuality(item);
                     }
@@ -73,45 +94,26 @@ public class GildedRose {
             }
             else
             {
-                if (isQualityAboveFifty(item))
+                if (isQualityLessThanFifty(item))
                 {
                     increaseQuality(item);
-
-                    if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
-                    {
-                        if (isSellInLessThanEleven(item))
-                        {
-                            if (isQualityAboveFifty(item))
-                            {
-                                increaseQuality(item);
-                            }
-                        }
-
-                        if (isSellInLessThanSix(item))
-                        {
-                            if (isQualityAboveFifty(item))
-                            {
-                                increaseQuality(item);
-                            }
-                        }
-                    }
                 }
             }
 
-            if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
+            if (!SULFURAS.equals(item.getName()))
             {
                 decreaseSellIn(item);
             }
 
             if (isSellInUnderZero(item))
             {
-                if (!"Aged Brie".equals(item.getName()))
+                if (!BRIE.equals(item.getName()))
                 {
-                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
+                    if (!BACKSTAGE.equals(item.getName()))
                     {
                         if (isQualityAboveZero(item))
                         {
-                            if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
+                            if (!SULFURAS.equals(item.getName()))
                             {
                                 decreaseQuality(item);
                             }
@@ -124,7 +126,7 @@ public class GildedRose {
                 }
                 else
                 {
-                    if (isQualityAboveFifty(item))
+                    if (isQualityLessThanFifty(item))
                     {
                         increaseQuality(item);
                     }
