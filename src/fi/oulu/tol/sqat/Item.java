336621d90133c957a5a7b80e60946e9020cbd2ee
skorkko <skorkko@STUPID-PC404.stnet.local>
2016-10-14 16:51:37 +0300

    refactored

--- src/fi/oulu/tol/sqat/Item.java
+++ src/fi/oulu/tol/sqat/Item.java
@@ -4,13 +4,26 @@ package fi.oulu.tol.sqat;
 public class Item {
     String name;
     int sellIn; 
-    int quality; 
+    int quality;
+    boolean developsNormally;
+    boolean decaysNormally;
     
     public Item(String name, int sellIn, int quality) {
 		this.setName(name);
 		this.setSellIn(sellIn);
 		this.setQuality(quality);
+		setFlags(name);
 	}
+    
+    private void setFlags(String name) {
+    	decaysNormally = true;
+    	developsNormally = true;
+    	if(name.equals("Aged Brie") || name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+    		developsNormally = false;
+    	} else if(name.equals("Sulfuras, Hand of Ragnaros")) {
+    		decaysNormally = false;
+    	} 
+    }
     
 	/* Generated getter and setter code */
     public String getName() {
@@ -31,5 +44,54 @@ public class Item {
 	public void setQuality(int quality) {
 		this.quality = quality;
 	}
+	
+	public void decreaseQuality() {
+		this.quality--;
+    }
+	
+	public void increaseQuality() {
+		this.quality++;
+	}
+	
+	public void decreaseSellin() {
+		if(name == "Sulfuras, Hand of Ragnaros") {
+			return;
+		}
+		this.sellIn--;
+	}
+	
+	public boolean isExpired() {
+		if(sellIn < 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean hasReachedMaximumQuality() {
+		if(name == "Sulfuras, Hand of Ragnaros") {
+			return true;
+		} else {
+			if(quality == 50) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean hasZeroQuality() {
+		if(quality == 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean decaysNormally() {
+		return decaysNormally;
+	}
+	
+	public boolean developsNormally() {
+		return developsNormally;
+	}
+	
 }
 
