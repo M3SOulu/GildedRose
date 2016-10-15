f061894cfb562eb63a09e3b69aeda4a4ec79465d
Joona Luodonpää <joona.luodonpaa@gmail.com>
2016-10-15 15:04:38 +0300

    Finish assignment

--- src/fi/oulu/tol/sqat/GildedRose.java
+++ src/fi/oulu/tol/sqat/GildedRose.java
@@ -6,6 +6,10 @@ import java.util.List;
 
 public class GildedRose {
 
+	public static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+	public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+	public static final String BRIE = "Aged Brie";
+	
 	private static List<Item> items = null;
 
 	public List<Item> getItems() {
@@ -19,80 +23,14 @@ public class GildedRose {
 	public GildedRose() {
 		items = new ArrayList<Item>();
 	}
+	
     public static void updateEndOfDay()
     {
-        for (int i = 0; i < items.size(); i++)
+        for (Item item : items)
         {
-            if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName())) 
-            {
-                if (items.get(i).getQuality() > 0)
-                {
-                    if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() - 1);
-                    }
-                }
-            }
-            else
-            {
-                if (items.get(i).getQuality() < 50)
-                {
-                    items.get(i).setQuality(items.get(i).getQuality() + 1);
-
-                    if ("Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
-                    {
-                        if (items.get(i).getSellIn() < 11)
-                        {
-                            if (items.get(i).getQuality() < 50)
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
-                            }
-                        }
-
-                        if (items.get(i).getSellIn() < 6)
-                        {
-                            if (items.get(i).getQuality() < 50)
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-            {
-                items.get(i).setSellIn(items.get(i).getSellIn() - 1);
-            }
-
-            if (items.get(i).getSellIn() < 0)
-            {
-                if (!"Aged Brie".equals(items.get(i).getName()))
-                {
-                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
-                    {
-                        if (items.get(i).getQuality() > 0)
-                        {
-                            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() - 1);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() - items.get(i).getQuality());
-                    }
-                }
-                else
-                {
-                    if (items.get(i).getQuality() < 50)
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() + 1);
-                    }
-                }
-            }
+        	item.updateQuality();
+            item.updateSellIn();
+            item.ensureBounds();
         }
     }
-
 }
--- src/fi/oulu/tol/sqat/Item.java
+++ src/fi/oulu/tol/sqat/Item.java
@@ -2,6 +2,11 @@ package fi.oulu.tol.sqat;
 
 
 public class Item {
+	
+	public static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+	public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+	public static final String BRIE = "Aged Brie";
+	
     String name;
     int sellIn; 
     int quality; 
@@ -16,20 +21,70 @@ public class Item {
     public String getName() {
 		return name;
 	}
+    
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public int getSellIn() {
 		return sellIn;
 	}
+	
 	public void setSellIn(int sellIn) {
 		this.sellIn = sellIn;
 	}
+	
 	public int getQuality() {
 		return quality;
 	}
+	
 	public void setQuality(int quality) {
 		this.quality = quality;
+	}
+	
+	public void updateQuality() {
+		int update = 1;
+		if (isExpired())
+			update = 2;
+		
+		if (BRIE.equals(name)) {
+			quality += update;
+		} else if (BACKSTAGE.equals(name)) {
+			if (isExpired())
+				quality = 0;
+			else {
+				if (sellIn <= 5)
+					update = 3;
+				else if (sellIn <= 10)
+					update = 2;
+				else
+					update = 1;
+				
+				quality += update;
+			}
+		} else {
+			quality -= update;
+		}
+	}
+	
+	public void updateSellIn() {
+		sellIn--;
+	}
+	
+	public void ensureBounds() {
+		if (SULFURAS.equals(name)) {
+			quality = 80;
+			sellIn = 0;
+		} else {
+			if (quality < 0)
+				quality = 0;
+			else if (quality > 50)
+				quality = 50;
+		}
+	}
+	
+	private boolean isExpired() {
+		return (sellIn <= 0);
 	}
 }
 
