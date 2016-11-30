d2aec73446097667b036ca3959fb3507676a1ff2
mat23795 <mat23795@hotmail.it>
2016-11-30 11:34:11 +0100

    Ragiunto il 100%

--- src/fi/oulu/tol/sqat/Item.java
+++ src/fi/oulu/tol/sqat/Item.java
@@ -1,35 +1,38 @@
 package fi.oulu.tol.sqat;
 
-
 public class Item {
-    private String name;
-    private int sellIn; 
-    private int quality; 
-    
-    public Item(String name, int sellIn, int quality) {
+	private String name;
+	private int sellIn;
+	private int quality;
+
+	public Item(String name, int sellIn, int quality) {
 		this.setName(name);
 		this.setSellIn(sellIn);
 		this.setQuality(quality);
 	}
-    
+
 	/* Generated getter and setter code */
-    public String getName() {
+	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getSellIn() {
 		return sellIn;
 	}
+
 	public void setSellIn(int sellIn) {
 		this.sellIn = sellIn;
 	}
+
 	public int getQuality() {
 		return quality;
 	}
+
 	public void setQuality(int quality) {
 		this.quality = quality;
 	}
 }
-
