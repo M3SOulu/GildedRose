8e4ee345ac5c023e5315d896e7294786c6e61561
zermo <zermo95@hotmail.it>
2016-12-02 17:56:30 +0100

    Refactoring del metodo update

--- src/fi/oulu/tol/sqat/GildedRose.java
+++ src/fi/oulu/tol/sqat/GildedRose.java
@@ -6,20 +6,20 @@ import java.util.List;
 
 public class GildedRose {
 
-	private static List<Item> items = null;
+    private static List<Item> items = null;
 
     public static List<Item> getItems() {
         return items;
     }
 
     /**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+     * @param args
+     */
+    public static void main(String[] args) {
 
         System.out.println("OMGHAI!");
 
-        items = new ArrayList<Item>();
+        items = new ArrayList<>();
         items.add(new Item("+5 Dexterity Vest", 10, 20));
         items.add(new Item("Aged Brie", 2, 0));
         items.add(new Item("Elixir of the Mongoose", 5, 7));
@@ -27,15 +27,59 @@ public class GildedRose {
         items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
         items.add(new Item("Conjured Mana Cake", 3, 6));
         items.add(new Item("SellInPassed", -1, 6));
-        items.add(new Item("QualityNegative", 2, -1));
 
         //updateQuality();
-}
+    }
 
 
+    public static void updateQuality() throws ItemException {
 
-    public static void updateQuality()
-    {
+        for (Item item : items) {
+            // The Quality of an item is never negative
+            if (item.getQuality() < 0) {
+                throw new ItemException();
+            }
+
+            String itemName = item.getName();
+            switch (itemName) {
+                // Aged Brie actually increases in Quality the older it gets (initialSellIn = 2)
+                case "Aged Brie":
+                    int daysSpent = -(item.getSellIn() - 2);
+                    item.setQuality(item.getQuality() + daysSpent);
+                    break;
+
+                // Sulfuras, being a legendary item, never has to be sold or decreases in Quality
+                case "Sulfuras, Hand of Ragnaros":
+                    break;
+
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    //Quality drops to 0 after the concert
+                    if (item.getSellIn() <= 0) {
+                        item.setQuality(0);
+                        break;
+                    }
+                    // Quality increases by 3 when there are 5 days or less
+                    if (item.getSellIn() <= 5) {
+                        item.setQuality(item.getQuality() + 3);
+                        break;
+                    }
+                    // Quality increases by 2 when there are 10 days or less
+                    if (item.getSellIn() <= 10) {
+                        item.setQuality(item.getQuality() + 2);
+                        break;
+                    }
+                    break;
+
+                default:
+                    // Once the SellIn date has passed, Quality degrades twice as fast
+                    if (item.getSellIn() <= 0) {
+                        item.setQuality(item.getQuality() - 2);
+                    }
+            }
+        }
+    }
+
+    /*public static void updateQualityOLD() {
         for (Item item : items) {
             if ((!"Aged Brie".equals(item.getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
                 if (item.getQuality() > 0) {
@@ -85,6 +129,6 @@ public class GildedRose {
                 }
             }
         }
-    }
+    }*/
 
 }
--- src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import fi.oulu.tol.sqat.GildedRose;
 import fi.oulu.tol.sqat.Item;
+import fi.oulu.tol.sqat.ItemException;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -25,38 +26,38 @@ public class GildedRoseTest {
 //        4 items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
 //        5 items.add(new Item("Conjured Mana Cake", 3, 6));
 //        6 items.add(new Item("SellInPassed", -1, 6));
-//        7 items.add(new Item("QualityNegative", 2, -1));
 
 
     }
 
     @Test
-    public void sellInPassedAndQualityDegradedTwice() throws Exception {
+    public void sellInPassedAndQualityDegradedTwice() throws Exception, ItemException {
         GildedRose.updateQuality();
         assertEquals(4, items.get(6).getQuality());
     }
 
-    /*@Test
-    public void qualityItemNeverNegative() throws Exception {
-        assertEquals(0, items.get(3).getQuality());
-    }*/
+    @Test (expected = ItemException.class)
+    public void qualityItemNeverNegative() throws ItemException {
+        items.get(1).setQuality(-10);
+        GildedRose.updateQuality();
+    }
 
     @Test
-    public void agedBriedIncreaseQualityOlderItGets1() throws Exception {
+    public void agedBrieIncreaseQualityOlderItGets1() throws Exception, ItemException {
         items.get(1).setSellIn(1);
         GildedRose.updateQuality();
         assertEquals(1, items.get(1).getQuality());
     }
 
     @Test
-    public void agedBriedIncreaseQualityOlderItGets2() throws Exception {
+    public void agedBrieIncreaseQualityOlderItGets2() throws Exception, ItemException {
         items.get(1).setSellIn(0);
         GildedRose.updateQuality();
         assertEquals(2, items.get(1).getQuality());
     }
 
     @Test
-    public void qualityNeverMoreThanFifty() throws Exception {
+    public void qualityNeverMoreThanFifty() throws Exception, ItemException {
         GildedRose.updateQuality();
         for (Item item : items) {
             // Sulfuras è leggendario quindi può avere la qualità maggiore di 50
@@ -67,14 +68,14 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void sulfurasNeverDecreaseQuality() throws Exception {
+    public void sulfurasNeverDecreaseQuality() throws Exception, ItemException {
         items.get(3).setSellIn(-10);
         GildedRose.updateQuality();
         assertEquals(80, items.get(3).getQuality());
     }
 
     @Test
-    public void backstageQualityIncreasedByTwoWhenSellInUpToTen() throws Exception {
+    public void backstageQualityIncreasedByTwoWhenSellInUpToTen() throws Exception, ItemException {
         items.get(4).setSellIn(10);
         GildedRose.updateQuality();
         assertEquals(22, items.get(4).getQuality());
@@ -88,7 +89,7 @@ public class GildedRoseTest {
     }*/
 
     @Test
-    public void backstageQualityIncreasedByThreeWhenSellInUpToFive() throws Exception {
+    public void backstageQualityIncreasedByThreeWhenSellInUpToFive() throws Exception, ItemException {
         items.get(4).setSellIn(5);
         GildedRose.updateQuality();
         assertEquals(23, items.get(4).getQuality());
@@ -102,7 +103,7 @@ public class GildedRoseTest {
     }*/
 
     @Test
-    public void backstageQualityDropsToZeroAfterTheConcert() throws Exception {
+    public void backstageQualityDropsToZeroAfterTheConcert() throws Exception, ItemException {
         items.get(4).setSellIn(-1);
         GildedRose.updateQuality();
         assertEquals(0, items.get(4).getQuality());
