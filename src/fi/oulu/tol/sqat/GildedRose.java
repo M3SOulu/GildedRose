d87d9747e44d39b4758eec9bc2b4a1ec2d2ac7ab
michelecafagna26 <mikele.c@outlook.it>
2016-12-03 19:56:44 +0100

    refactor of updateQuality

--- src/fi/oulu/tol/sqat/GildedRose.java
+++ src/fi/oulu/tol/sqat/GildedRose.java
@@ -1,68 +1,102 @@
 package fi.oulu.tol.sqat;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class GildedRose {
 
 	public static List<Item> items = null;
 	
+	private static final String[] SPECIAL_ITEM={"Normal","Aged Brie","Sulfuras","Backstage passes"," Conjured"};
+
 	public GildedRose(){
 		
 		items= new ArrayList<Item>();
 	}
 	
 	public static void updateQuality() {
-		for (int i = 0; i < items.size(); i++) {
-			if ((!"Aged Brie".equals(items.get(i).getName()))
-					&& !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName())) {
-				if (items.get(i).getQuality() > 0) {
-					if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName())) {
-						items.get(i).setQuality(items.get(i).getQuality() - 1);
-					}
-				}
-			} else {
-				if (items.get(i).getQuality() < 50) {
-					items.get(i).setQuality(items.get(i).getQuality() + 1);
-
-					if ("Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName())) {
-						if (items.get(i).getSellIn() < 11) {
-							if (items.get(i).getQuality() < 50) {
-								items.get(i).setQuality(items.get(i).getQuality() + 1);
-							}
-						}
-
-						if (items.get(i).getSellIn() < 6) {
-							if (items.get(i).getQuality() < 50) {
-								items.get(i).setQuality(items.get(i).getQuality() + 1);
-							}
-						}
-					}
-				}
+		
+		Iterator<Item> it=	items.iterator();
+		
+		while(it.hasNext()){
+			
+			Item item=it.next();
+			
+			String type=specialItemDetector(item);
+			
+			if(type.equals(SPECIAL_ITEM[0])){
+				
+				updateNormalItem(item);
+		
+			}else if(type.equals(SPECIAL_ITEM[1])){
+				
+				updateAgedBrieItem(item);
 			}
-
-			if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName())) {
-				items.get(i).setSellIn(items.get(i).getSellIn() - 1);
-			}
-
-			if (items.get(i).getSellIn() < 0) {
-				if (!"Aged Brie".equals(items.get(i).getName())) {
-					if (!"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName())) {
-						if (items.get(i).getQuality() > 0) {
-							if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName())) {
-								items.get(i).setQuality(items.get(i).getQuality() - 1);
-							}
-						}
-					} else {
-						items.get(i).setQuality(items.get(i).getQuality() - items.get(i).getQuality());
-					}
-				} else {
-					if (items.get(i).getQuality() < 50) {
-						items.get(i).setQuality(items.get(i).getQuality() + 1);
-					}
-				}
-			}
+			
+			
 		}
 	}
+
+/**
+ * get the item type	
+ * @param item not null
+ * @return one value from SPECIAL_ITEM vector
+ */
+ static String specialItemDetector(Item item){
+		
+		String type="";
+		
+		int index=1;
+		
+		while(!item.getName().contains(SPECIAL_ITEM[index]) && index<SPECIAL_ITEM.length-1){
+			
+			index++;
+		}
+		
+		if(index==SPECIAL_ITEM.length-1){
+			
+			type=SPECIAL_ITEM[0];
+			
+		}else{
+			
+			type=SPECIAL_ITEM[index];
+			
+		}
+		return type;
+	}
+	
+ 	/**
+ 	 * update quality of an item
+ 	 * @param item not null
+ 	 * @param type , elements of SPECIAL_ITEM list of types
+ 	 */
+	private static void updateNormalItem(Item item){
+		
+			item.setSellIn(item.getSellIn()-1);
+		
+			if(item.getSellIn()>=0){
+				
+				if(item.getQuality()>0)
+					item.setQuality(item.getQuality()-1);
+				
+			}else{
+				
+				item.setQuality(item.getQuality()-2);
+			}
+			
+			if(item.getQuality()<0)
+				item.setQuality(0);
+	}
+	
+	/**
+	 * update quality of an Aged Brie item
+	 * @param item
+	 */
+	private static void updateAgedBrieItem(Item item){
+		
+		item.setSellIn(item.getSellIn()-1);
+		item.setQuality(item.getQuality()+1);
+	}	
 
 }
